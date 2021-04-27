58f8fcd88536151abf17ef6eea1c617882ec97bd
viniciussslima999 <vinicius.silva.017@ufrn.edu.br>
2021-04-26 22:41:56 -0300

    Configurando conexão com qualquer rede

--- front-end/src/web3/injected.jsx
+++ front-end/src/web3/injected.jsx
@@ -1,7 +1,5 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
 
-const injected = new InjectedConnector({
-  supportedChainIds: [1, 3, 4, 5, 42],
-});
+const injected = new InjectedConnector({});
 
 export default injected;
